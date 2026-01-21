ae9f00acf057dd673fec5af890ae4a2766d027ea
Milan Vlaški <milan.vlaski321@gmail.com>
2026-01-21 14:44:44 +0100

    feat: add initial index.html. and makefile to run it with

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",    // Eleventy will look here for files
+      output: "_site"  // Eleventy will put the built files here
+    }
+  }
+}
