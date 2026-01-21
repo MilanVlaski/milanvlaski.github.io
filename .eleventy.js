698760e4ceeeb3c13aa0388cd3925d8839bade82
Milan Vlaški <milan.vlaski321@gmail.com>
2026-01-21 15:09:59 +0100

    feat: add css

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function(eleventyConfig) {
   // Copy CSS files to the output directory
-  eleventyConfig.addPassthroughCopy({"src/*.*": "."});
+  eleventyConfig.addPassthroughCopy({"src/*.*": "."})
+  eleventyConfig.addPassthroughCopy({"src/assets/css": "assets/css"})
 
   return {
     dir: {
-      input: "src",    // Eleventy will look here for files
-      output: "_site"  // Eleventy will put the built files here
+      input: "src",
+      output: "_site",
     }
   }
 }
