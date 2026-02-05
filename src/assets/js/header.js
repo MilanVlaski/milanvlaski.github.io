11db9e30ea16bea55e4a5a726ba7cfa7c8359264
Milan Vlaški <milan.vlaski321@gmail.com>
2026-02-05 15:37:54 +0100

    feat: move styling to web component

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -2,14 +2,13 @@ class MyHeader extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = /*html*/`
-        <nav style="display: flex; justify-content: space-between;">
+        <nav>
             <a href="/">Home</a>
-
-        <ul style="list-style: none; display: flex; gap: 0.5rem; flex-direction: row;">
-            <li><a href="/writing">Writing</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/writing/my_work">My Services</a></li>
-        </ul>
+            <ul>
+                <li><a href="/writing">Writing</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/writing/my_work">My Services</a></li>
+            </ul>
         </nav>
     `
     }
